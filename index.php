c2ee1f2bb2f13ea6466939a2cca35eb0ad41319b
B4R4N0VS <anton2444927@gmail.com>
2022-02-28 16:52:58 +0300

    v1.1

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Baranov_Anton_211-321</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+</head>
+<body>
+    <header class="header">
+        <section class="header__content">
+            <img src="./pics/logo.jpg" alt="Mospolytech" class="header__content_logo">
+            <h1>Лабораторная работа №1 "Hello world!"</h1>
+        </section>
+    </header>
+
+    <?php echo "
+        <main>
+            <div class='main__content'>
+                <p class='main__content_text'>Hello, World!</p>
+            </div>
+        </main>    
+    "
+    ?>
+
+    <footer class="footer">
+        <div class="footer__content">
+            <p class="footer__conent_name">Баранов Антон Андреевич</p>
+            <p class="footer__content_group">211-231</p>
+        </div>
+    </footer>
+</body>
+</html>
